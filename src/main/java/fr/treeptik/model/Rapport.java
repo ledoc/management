20391b4faac989217e7385d831dcf7a9baadb41f
herve <h.fontbonne@treeptik.fr>
2015-01-14 10:59:35 +0100

    Ajout des objets de travail

---
+++ src/main/java/fr/treeptik/model/Rapport.java
@@ -0,0 +1,61 @@
+package fr.treeptik.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Rapport {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	private Date date;
+	private String nom;
+	private Integer taille;
+	private Ouvrage ouvrage;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Integer getTaille() {
+		return taille;
+	}
+
+	public void setTaille(Integer taille) {
+		this.taille = taille;
+	}
+
+	public Ouvrage getOuvrage() {
+		return ouvrage;
+	}
+
+	public void setOuvrage(Ouvrage ouvrage) {
+		this.ouvrage = ouvrage;
+	}
+}
---
+++ src/main/java/fr/treeptik/model/TrameDW.java
@@ -0,0 +1,80 @@
+package fr.treeptik.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TrameDW {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private Date date;
+	private String heure;
+	// milliAmpère
+	private String signalBrut;
+	// 1ere conversion
+	private String hauteurEau;
+	// niveau d’eau en mètre au moment de l’installation du capteur et de la
+	// mesure manuelle de référence Nm0
+	private String niveauStatique;
+	// 2ème conversion
+	private String coteAltimetrique;
+	private List<Alerte> alertes;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getHeure() {
+		return heure;
+	}
+	public void setHeure(String heure) {
+		this.heure = heure;
+	}
+	public String getSignalBrut() {
+		return signalBrut;
+	}
+	public void setSignalBrut(String signalBrut) {
+		this.signalBrut = signalBrut;
+	}
+	public String getHauteurEau() {
+		return hauteurEau;
+	}
+	public void setHauteurEau(String hauteurEau) {
+		this.hauteurEau = hauteurEau;
+	}
+	public String getNiveauStatique() {
+		return niveauStatique;
+	}
+	public void setNiveauStatique(String niveauStatique) {
+		this.niveauStatique = niveauStatique;
+	}
+	public String getCoteAltimetrique() {
+		return coteAltimetrique;
+	}
+	public void setCoteAltimetrique(String coteAltimetrique) {
+		this.coteAltimetrique = coteAltimetrique;
+	}
+	public List<Alerte> getAlertes() {
+		return alertes;
+	}
+	public void setAlertes(List<Alerte> alertes) {
+		this.alertes = alertes;
+	}
+
+	
+	
+}
