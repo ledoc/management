54e6caceee985ae7c932e302d9c1dccb6c581ef6
herve <h.fontbonne@treeptik.fr>
2015-02-04 23:27:41 +0100

    attribut de session pour enregistreur

--- src/main/java/fr/treeptik/controller/EnregistreurController.java
+++ src/main/java/fr/treeptik/controller/EnregistreurController.java
@@ -24,19 +24,23 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import fr.treeptik.exception.ControllerException;
 import fr.treeptik.exception.ServiceException;
 import fr.treeptik.model.Alerte;
 import fr.treeptik.model.Enregistreur;
 import fr.treeptik.model.Mesure;
+import fr.treeptik.model.Ouvrage;
 import fr.treeptik.model.TypeMesure;
 import fr.treeptik.service.AlerteService;
 import fr.treeptik.service.EnregistreurService;
 import fr.treeptik.service.MesureService;
+import fr.treeptik.service.OuvrageService;
 
 @Controller
 @RequestMapping("/enregistreur")
+@SessionAttributes("enregistreur")
 public class EnregistreurController {
 
 	private Logger logger = Logger.getLogger(EnregistreurController.class);
@@ -44,22 +48,66 @@ public class EnregistreurController {
 	@Inject
 	private EnregistreurService enregistreurService;
 	@Inject
+	private OuvrageService ouvrageService;
+	@Inject
 	private MesureService mesureService;
 	@Inject
 	private AlerteService alerteService;
-	private Map<Integer, Alerte> alerteCache;
 
-	@RequestMapping(method = RequestMethod.GET, value = "/create")
-	public String create(Model model) throws ControllerException {
-		logger.info("--create formulaire EnregistreurController--");
+	@RequestMapping(value = "/init", method = RequestMethod.POST)
+	public String initEnregistreur(@ModelAttribute Enregistreur enregistreur,
+			BindingResult result, Model model) throws ControllerException {
+
+		logger.debug(result.getAllErrors());
+		logger.info("--create EnregistreurController-- " + enregistreur);
+
+		List<Alerte> alertesCombo;
+		Map<Integer, Alerte> alerteCache;
 
 		List<TypeMesure> typesMesureCombo = new ArrayList<TypeMesure>(
 				Arrays.asList(TypeMesure.values()));
-		List<Alerte> alertesCombo;
+		alerteCache = new HashMap<Integer, Alerte>();
 
 		try {
-			// ne renvoie que les alertes libres
+
 			alertesCombo = alerteService.findAll();
+
+			for (Alerte alerte : alertesCombo) {
+				alerteCache.put(alerte.getId(), alerte);
+			}
+		} catch (NumberFormatException | ServiceException e) {
+			logger.error(e.getMessage());
+			throw new ControllerException(e.getMessage(), e);
+		}
+
+		model.addAttribute("enregistreur", enregistreur);
+		model.addAttribute("typesMesureCombo", typesMesureCombo);
+		model.addAttribute("alertesCombo", alertesCombo);
+
+		return "/enregistreur/create";
+
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/create/{ouvrageId}")
+	public String create(Model model,
+			@PathVariable("ouvrageId") Integer ouvrageId)
+			throws ControllerException {
+		logger.info("--create formulaire EnregistreurController-- ouvrageId = "
+				+ ouvrageId);
+
+		Enregistreur enregistreur = new Enregistreur();
+		Ouvrage ouvrage = new Ouvrage();
+		List<TypeMesure> typesMesureCombo = new ArrayList<TypeMesure>(
+				Arrays.asList(TypeMesure.values()));
+		List<Alerte> alertesCombo;
+		Map<Integer, Alerte> alerteCache;
+
+		try {
+			ouvrage = ouvrageService.findById(ouvrageId);
+			alertesCombo = alerteService.findAll();
+
+			enregistreur.setOuvrage(ouvrage);
+
 		} catch (ServiceException e) {
 			logger.error(e.getMessage());
 			throw new ControllerException(e.getMessage(), e);
@@ -70,11 +118,53 @@ public class EnregistreurController {
 			alerteCache.put(alerte.getId(), alerte);
 		}
 
-		model.addAttribute("enregistreur", new Enregistreur());
+		model.addAttribute("enregistreur", enregistreur);
 		model.addAttribute("mesure", new Mesure());
 		model.addAttribute("typesMesureCombo", typesMesureCombo);
 		model.addAttribute("alertesCombo", alertesCombo);
 		return "/enregistreur/create";
+	}
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public String create(@ModelAttribute Enregistreur enregistreur,
+			BindingResult result) throws ControllerException {
+		logger.debug(result.getAllErrors());
+		logger.info("--create EnregistreurController--");
+		logger.debug("enregistreur : " + enregistreur);
+		logger.debug(" enregistreur Mesure Enregistreur "
+				+ enregistreur.getMesureEnregistreur());
+		Ouvrage ouvrage = new Ouvrage();
+		try {
+
+			ouvrage = ouvrageService
+					.findByIdWithJoinFetchEnregistreurs(enregistreur
+							.getOuvrage().getId());
+
+			// TODO HACK A supprimer
+			if (enregistreur.getMesureEnregistreur() != null
+					&& enregistreur.getMesureEnregistreur().getId() == null) {
+				enregistreur.setMesureEnregistreur(null);
+			}
+			if (enregistreur.getNiveauManuel() != null
+					&& enregistreur.getNiveauManuel().getId() == null) {
+				Mesure niveauManuel = mesureService.create(enregistreur
+						.getNiveauManuel());
+				enregistreur = enregistreurService.create(enregistreur);
+				niveauManuel.setEnregistreur(enregistreur);
+				mesureService.update(niveauManuel);
+			} else {
+				enregistreur = enregistreurService.create(enregistreur);
+			}
+			if (!ouvrage.getEnregistreurs().contains(enregistreur)) {
+				ouvrage.getEnregistreurs().add(enregistreur);
+				ouvrageService.update(ouvrage);
+			}
+
+		} catch (ServiceException e) {
+			logger.error(e.getMessage());
+			throw new ControllerException(e.getMessage(), e);
+		}
+		return "redirect:/ouvrage/list";
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/update/{id}")
@@ -85,6 +175,8 @@ public class EnregistreurController {
 
 		Enregistreur enregistreur = null;
 		List<Alerte> alertesCombo;
+		Map<Integer, Alerte> alerteCache;
+
 		List<TypeMesure> typesMesureCombo = new ArrayList<TypeMesure>(
 				Arrays.asList(TypeMesure.values()));
 		alerteCache = new HashMap<Integer, Alerte>();
@@ -114,38 +206,20 @@ public class EnregistreurController {
 			throws ControllerException {
 		logger.info("--delete EnregistreurController--");
 		logger.debug("enregistreurId : " + id);
-
+		Integer ouvrageId;
 		try {
+
+			// if(enregistreurService.findById(id).getMesures() != null) {
+			// enre
+			// }
+			ouvrageId = enregistreurService.findById(id).getOuvrage().getId();
+
 			enregistreurService.remove(id);
 		} catch (NumberFormatException | ServiceException e) {
 			logger.error(e.getMessage());
 			throw new ControllerException(e.getMessage(), e);
 		}
-		return "redirect:/enregistreur/list";
-	}
-
-	@RequestMapping(value = "/create", method = RequestMethod.POST)
-	public String create(@ModelAttribute Enregistreur enregistreur,
-			BindingResult result) throws ControllerException {
-		logger.debug(result.getAllErrors());
-		logger.info("--create EnregistreurController--");
-		logger.debug("enregistreur : " + enregistreur);
-		logger.info(enregistreur.getMesureEnregistreur().getValeur());
-		try {
-
-			/**
-			 * TODO : comprendre pourquoi il faut sauver un objet null
-			 * :(((((((((
-			 */
-			mesureService.create(enregistreur.getMesureEnregistreur());
-
-			enregistreurService.create(enregistreur);
-		} catch (ServiceException e) {
-			logger.error(e.getMessage());
-			throw new ControllerException(e.getMessage(), e);
-		}
-		return "redirect:/enregistreur/list";
-
+		return "redirect:/ouvrage/update/" + ouvrageId ;
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/list")
@@ -177,7 +251,7 @@ public class EnregistreurController {
 
 	@RequestMapping(value = "/update/niveau/manuel", method = RequestMethod.POST)
 	public String updateNiveauManuel(@ModelAttribute Enregistreur enregistreur,
-			BindingResult result, Model model) throws ControllerException {
+			BindingResult result) throws ControllerException {
 		logger.info(result.getGlobalError());
 
 		logger.info("--updateNiveauManuel EnregistreurController--");
@@ -192,7 +266,7 @@ public class EnregistreurController {
 			enregistreur = enregistreurService.create(enregistreur);
 			mesure.setEnregistreur(enregistreur);
 			mesureService.update(mesure);
-			
+
 			logger.info("enregistreur.getNiveauManuel() : "
 					+ enregistreur.getNiveauManuel());
 		} catch (ServiceException e) {
@@ -212,6 +286,8 @@ public class EnregistreurController {
 		binder.registerCustomEditor(List.class, "alertes",
 				new CustomCollectionEditor(List.class) {
 					protected Object convertElement(Object element) {
+						Map<Integer, Alerte> alerteCache = new HashMap<Integer, Alerte>();
+
 						if (element instanceof Alerte) {
 							logger.info("Conversion d'un Alerte en Alerte: "
 									+ element);
@@ -235,14 +311,6 @@ public class EnregistreurController {
 						return null;
 					}
 				});
-	}
-
-	public Map<Integer, Alerte> getAlerteCache() {
-		return alerteCache;
-	}
-
-	public void setAlerteCache(Map<Integer, Alerte> alerteCache) {
-		this.alerteCache = alerteCache;
 	}
 
 }
