8f625d6af862cf4c60967f75ea2392c8b0119773
herve <h.fontbonne@treeptik.fr>
2015-02-09 21:59:54 +0100

    meth de dao pour validator unicite

--- src/main/java/fr/treeptik/dao/ClientDAO.java
+++ src/main/java/fr/treeptik/dao/ClientDAO.java
@@ -14,4 +14,16 @@ public interface ClientDAO extends JpaRepository<Client, Integer> {
 
 	@Query("select c from Client c left join fetch c.etablissements e join e.sites s join s.ouvrages o where o.id = :ouvrageId")
 	public Client findClientByOuvrageId(@Param("ouvrageId") Integer ouvrageId) throws DataAccessException;
+
+	
+	@Query("select c from Client c left join fetch c.documents where c.id = :id")
+	public Client findByIdWithJoinFetchDocuments(@Param("id") Integer id) throws DataAccessException;
+
+	public Long countByLogin(String login);
+
+	public Long countByIdentifiant(String identifiant);
+
+	@Query("select count(c) from Client c Where c.login = :login and c.id != :id")
+	public Long countByLoginAndID(@Param("login") String login, @Param("id") Integer id);
+
 }
